9b4a6581ae872e8448b60efd11049b1f6e71ef4e
TimothySteinert <timothy@steinert.net.au>
2025-09-01 00:09:28 +0800

    Update pico_expander.h

--- components/pico_expander/pico_expander.h
+++ components/pico_expander/pico_expander.h
@@ -1,7 +1,7 @@
+// pico_expander.h
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/core/hal.h"
 #include "esphome/components/i2c/i2c.h"
 
 namespace esphome {
@@ -12,40 +12,11 @@ class PicoExpanderComponent : public Component, public i2c::I2CDevice {
   PicoExpanderComponent() = default;
 
   void setup() override;
-  void loop() override;
   void dump_config() override;
 
-  bool digital_read(uint8_t pin);
-  void digital_write(uint8_t pin, bool value);
-  void pin_mode(uint8_t pin, gpio::Flags flags);
+  void write_rgb(uint8_t r, uint8_t g, uint8_t b);
 
   float get_setup_priority() const override { return setup_priority::IO; }
-
- protected:
-  uint8_t last_state_[2]{0x00, 0x00};
-};
-
-class PicoExpanderGPIOPin : public GPIOPin {
- public:
-  void setup() override;
-  void pin_mode(gpio::Flags flags) override;
-
-  bool digital_read() override;
-  void digital_write(bool value) override;
-  std::string dump_summary() const override;
-
-  void set_parent(PicoExpanderComponent *parent) { this->parent_ = parent; }
-  void set_pin(uint8_t pin) { this->pin_ = pin; }
-  void set_inverted(bool inverted) { this->inverted_ = inverted; }
-  void set_flags(gpio::Flags flags) { this->flags_ = flags; }
-  gpio::Flags get_flags() const override { return this->flags_; }
-
- protected:
-  PicoExpanderComponent *parent_;
-  uint8_t pin_;
-  bool inverted_{false};
-  bool setup_{false};
-  gpio::Flags flags_;
 };
 
 }  // namespace pico_expander
