7bc529628d329f6e2ca7405edbdcf7583ea0ef99
TimothySteinert <timothy@steinert.net.au>
2025-09-01 00:15:05 +0800

    Update __init__.py

--- components/pico_expander/__init__.py
+++ components/pico_expander/__init__.py
@@ -29,7 +29,7 @@ async def to_code(config):
 # Output schema
 OUTPUT_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend(
     {
-        cv.GenerateID(): cv.declare_id(PicoExpanderOutput),
+        cv.GenerateID(CONF_ID): cv.declare_id(PicoExpanderOutput),
         cv.Required(CONF_PICO_EXPANDER): cv.use_id(PicoExpanderComponent),
         cv.Required(CONF_NUMBER): cv.int_range(min=0, max=2),
     }
@@ -37,7 +37,7 @@ OUTPUT_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend(
 
 @output.register_output("pico_expander", PicoExpanderOutput, OUTPUT_SCHEMA)
 async def output_to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = cg.new_Pvariable(config[CONF_ID])  # <- this ID is for PicoExpanderOutput now
     parent = await cg.get_variable(config[CONF_PICO_EXPANDER])
     cg.add(var.set_parent(parent))
     cg.add(var.set_channel(config[CONF_NUMBER]))
