09df26af8368d3888a074ae14c14c44b07108b50
TimothySteinert <timothy@steinert.net.au>
2025-09-01 00:12:38 +0800

    Update pico_expander.cpp

--- components/pico_expander/pico_expander.cpp
+++ components/pico_expander/pico_expander.cpp
@@ -1,4 +1,3 @@
-// pico_expander.cpp
 #include "pico_expander.h"
 #include "esphome/core/log.h"
 
@@ -7,13 +6,8 @@ namespace pico_expander {
 
 static const char *const TAG = "pico_expander";
 
-// Register map:
-// 0x00 = Red
-// 0x01 = Green
-// 0x02 = Blue
-
 void PicoExpanderComponent::setup() {
-  ESP_LOGCONFIG(TAG, "Setting up PicoExpander (RGB) at 0x%02X ...", this->address_);
+  ESP_LOGCONFIG(TAG, "Setting up PicoExpander RGB at 0x%02X ...", this->address_);
 }
 
 void PicoExpanderComponent::dump_config() {
@@ -24,9 +18,9 @@ void PicoExpanderComponent::dump_config() {
   }
 }
 
-void PicoExpanderComponent::write_rgb(uint8_t r, uint8_t g, uint8_t b) {
-  uint8_t buf[3] = {r, g, b};
-  if (this->write_register(0x00, buf, 3) != i2c::ERROR_OK) {
+void PicoExpanderComponent::write_value(uint8_t channel, uint8_t value) {
+  if (channel > 2) return;  // only 3 channels
+  if (this->write_register(channel, &value, 1) != i2c::ERROR_OK) {
     this->status_set_warning();
   }
 }
