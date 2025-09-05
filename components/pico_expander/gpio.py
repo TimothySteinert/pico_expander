dcd97712786dd613530d206fe9e38f73046c896d
TimothySteinert <timothy@steinert.net.au>
2025-09-05 14:24:35 +0800

    Update gpio.py

--- components/pico_expander/gpio.py
+++ components/pico_expander/gpio.py
@@ -8,17 +8,16 @@ from . import PicoExpanderComponent, CONF_PICO_EXPANDER
 pico_expander_ns = cg.esphome_ns.namespace("pico_expander")
 PicoExpanderGPIOPin = pico_expander_ns.class_("PicoExpanderGPIOPin", cg.GPIOPin)
 
-CONFIG_SCHEMA = cv.Schema(
+PICO_EXPANDER_PIN_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(PicoExpanderGPIOPin),
         cv.Required(CONF_PICO_EXPANDER): cv.use_id(PicoExpanderComponent),
         cv.Required(CONF_NUMBER): cv.int_range(min=0x40, max=0x4F),
-        # no need for input/pullup — these are always outputs
     }
 )
 
-@pins.PIN_SCHEMA_REGISTRY.register(CONF_PICO_EXPANDER, CONFIG_SCHEMA)
-async def to_code(config):
+@pins.PIN_SCHEMA_REGISTRY.register(CONF_PICO_EXPANDER, PICO_EXPANDER_PIN_SCHEMA)
+async def pico_expander_pin_to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     parent = await cg.get_variable(config[CONF_PICO_EXPANDER])
     cg.add(var.set_parent(parent))
