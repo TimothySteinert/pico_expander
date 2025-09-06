dbd8060a1b84041685dea223828d3f776f699d3e
TimothySteinert <timothy@steinert.net.au>
2025-09-06 16:58:20 +0800

    Create k1_keypad_alarm_state.h

---
+++ components/k1_keypad_alarm_state/k1_keypad_alarm_state.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "esphome/core/component.h"
+#include "esphome/components/text_sensor/text_sensor.h"
+
+namespace esphome {
+namespace k1_keypad_alarm_state {
+
+class K1KeypadAlarmState : public Component, public text_sensor::TextSensor {
+ public:
+  void set_entity_id(const std::string &entity_id) { this->entity_id_ = entity_id; }
+
+  void setup() override;
+  void dump_config() override;
+  void loop() override;
+
+  void process_state(const std::string &state);
+
+ protected:
+  std::string entity_id_;
+};
+
+}  // namespace k1_keypad_alarm_state
+}  // namespace esphome
