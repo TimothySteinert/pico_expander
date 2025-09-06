b0ee9d075bf1bd765b0c0d608e9f2ebf1f2bd2fa
TimothySteinert <timothy@steinert.net.au>
2025-09-06 20:45:18 +0800

    Update k1_keypad_alarm_state.h

--- components/k1_keypad_alarm_state/k1_keypad_alarm_state.h
+++ components/k1_keypad_alarm_state/k1_keypad_alarm_state.h
@@ -1,24 +1,23 @@
 #pragma once
 #include "esphome/core/component.h"
 #include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/components/api/custom_api_device.h"
 
 namespace esphome {
 namespace k1_keypad_alarm_state {
 
-class K1KeypadAlarmState : public Component, public text_sensor::TextSensor {
+class K1KeypadAlarmState : public Component,
+                           public text_sensor::TextSensor,
+                           public api::CustomAPIDevice {
  public:
-  void set_entity_id(const std::string &entity_id) { this->entity_id_ = entity_id; }
-  void set_name(const std::string &name) { this->name_ = name; }
+  void set_entity_id(const std::string &entity_id) { entity_id_ = entity_id; }
 
   void setup() override;
   void dump_config() override;
-  void loop() override;
-
-  void process_state(const std::string &state);
 
  protected:
+  void on_ha_state_(std::string state);
   std::string entity_id_;
-  std::string name_;
 };
 
 }  // namespace k1_keypad_alarm_state
