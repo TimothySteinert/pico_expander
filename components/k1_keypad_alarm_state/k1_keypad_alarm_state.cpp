a54696621c55ae2542b484e4358e537d94740009
TimothySteinert <timothy@steinert.net.au>
2025-09-06 20:44:58 +0800

    Update k1_keypad_alarm_state.cpp

--- components/k1_keypad_alarm_state/k1_keypad_alarm_state.cpp
+++ components/k1_keypad_alarm_state/k1_keypad_alarm_state.cpp
@@ -1,6 +1,5 @@
 #include "k1_keypad_alarm_state.h"
 #include "esphome/core/log.h"
-#include "esphome/components/homeassistant/text_sensor/homeassistant_text_sensor.h"
 
 namespace esphome {
 namespace k1_keypad_alarm_state {
@@ -10,38 +9,19 @@ static const char *const TAG = "k1_keypad_alarm_state";
 void K1KeypadAlarmState::setup() {
   ESP_LOGI(TAG, "Subscribing to HA entity: %s", this->entity_id_.c_str());
 
-  auto *ha_sensor = new homeassistant::HomeassistantTextSensor();
-  ha_sensor->set_entity_id(this->entity_id_);
-  ha_sensor->add_on_state_callback([this](std::string state) {
-    this->process_state(state);
-  });
-
-  App.register_component(ha_sensor);
-
-  // Register the name with ESPHome’s entity system
-  if (!this->name_.empty()) {
-    this->set_name(this->name_);
-  }
+  // Requires api: homeassistant_states: true in YAML
+  this->subscribe_homeassistant_state(&K1KeypadAlarmState::on_ha_state_, this->entity_id_);
 }
 
 void K1KeypadAlarmState::dump_config() {
   ESP_LOGCONFIG(TAG, "K1 Keypad Alarm State:");
   ESP_LOGCONFIG(TAG, "  Entity ID: %s", this->entity_id_.c_str());
-  ESP_LOGCONFIG(TAG, "  Name: %s", this->name_.c_str());
 }
 
-void K1KeypadAlarmState::loop() {
-  // nothing – all updates come via callback
-}
-
-void K1KeypadAlarmState::process_state(const std::string &state) {
-  std::string mapped;
-  if (state == "unavailable" || state == "unknown") {
-    mapped = "connection_timed_out";
-  } else {
-    mapped = state;
-  }
-
+void K1KeypadAlarmState::on_ha_state_(std::string state) {
+  std::string mapped = (state == "unavailable" || state == "unknown")
+                         ? "connection_timed_out"
+                         : state;
   ESP_LOGD(TAG, "Mapped state: %s -> %s", state.c_str(), mapped.c_str());
   this->publish_state(mapped);
 }
