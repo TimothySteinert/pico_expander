299d9e2e5d41915a75d429b069ecfa860caceeae
TimothySteinert <timothy@steinert.net.au>
2025-09-10 22:25:17 +0800

    Create buzzer_mute_switch.cpp

---
+++ components/buzzer/switch/buzzer_mute_switch.cpp
@@ -0,0 +1,10 @@
+#include "buzzer_mute_switch.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace buzzer {
+
+// (No extra runtime logic required here presently)
+
+}  // namespace buzzer
+}  // namespace esphome
