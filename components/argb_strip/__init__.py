b1bfe1e920865bec5b6b948bd04229cbe180f860
TimothySteinert <timothy@steinert.net.au>
2025-09-10 16:17:09 +0800

    Update __init__.py

--- components/argb_strip/__init__.py
+++ components/argb_strip/__init__.py
@@ -5,9 +5,6 @@ from esphome.const import CONF_ID, CONF_PIN
 
 CONF_NUM_LEDS = "num_leds"
 CONF_GROUPS = "groups"
-CONF_GLOBAL_BRIGHTNESS = "global_brightness"
-
-# Per-group keys
 CONF_LEDS = "leds"
 CONF_MAX_BRIGHTNESS = "max_brightness"
 
@@ -15,16 +12,15 @@ argb_strip_ns = cg.esphome_ns.namespace("argb_strip")
 ARGBStripComponent = argb_strip_ns.class_("ARGBStripComponent", cg.Component)
 
 def _validate_group_block(value):
-    # Accept either a bare list (just LEDs) or a dict with leds + max_brightness
     if isinstance(value, list):
         return {CONF_LEDS: value, CONF_MAX_BRIGHTNESS: 1.0}
     if isinstance(value, dict):
         if CONF_LEDS not in value:
             raise cv.Invalid(f"Group dict must contain '{CONF_LEDS}' key")
         leds = cv.ensure_list(cv.int_range(min=1))(value[CONF_LEDS])
-        max_brt = value.get(CONF_MAX_BRIGHTNESS, 1.0)
-        max_brt = cv.float_range(min=0.0, max=1.0)(max_brt)
-        return {CONF_LEDS: leds, CONF_MAX_BRIGHTNESS: max_brt}
+        mb = value.get(CONF_MAX_BRIGHTNESS, 1.0)
+        mb = cv.float_range(min=0.0, max=1.0)(mb)
+        return {CONF_LEDS: leds, CONF_MAX_BRIGHTNESS: mb}
     raise cv.Invalid("Group value must be a list of LED numbers or a dict with 'leds' and optional 'max_brightness'")
 
 def groups_schema(value):
@@ -42,7 +38,6 @@ CONFIG_SCHEMA = cv.Schema(
         cv.Required(CONF_PIN): pins.internal_gpio_output_pin_schema,
         cv.Required(CONF_NUM_LEDS): cv.int_range(min=1, max=2048),
         cv.Required(CONF_GROUPS): groups_schema,
-        cv.Optional(CONF_GLOBAL_BRIGHTNESS, default=1.0): cv.float_range(min=0.0, max=1.0),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -55,7 +50,6 @@ async def to_code(config):
     num_leds = config[CONF_NUM_LEDS]
     cg.add(var.set_num_leds(num_leds))
     cg.add(var.set_raw_gpio(config[CONF_PIN]['number']))
-    cg.add(var.set_global_brightness(config[CONF_GLOBAL_BRIGHTNESS]))
 
     for name, grp in config[CONF_GROUPS].items():
         leds = grp[CONF_LEDS]
