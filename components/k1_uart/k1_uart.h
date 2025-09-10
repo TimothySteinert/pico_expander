a1003186b5356218727200204768bce1b1b9e9bd
TimothySteinert <timothy@steinert.net.au>
2025-09-10 23:03:13 +0800

    Update k1_uart.h

--- components/k1_uart/k1_uart.h
+++ components/k1_uart/k1_uart.h
@@ -6,8 +6,20 @@
 #include "driver/uart.h"
 #endif
 
+#include <array>
+#include <cstdint>
+
 namespace esphome {
 namespace k1_uart {
+
+/*
+  K1UartComponent
+  - Hard-coded UART1 (TX=GPIO33, RX=GPIO32) 115200 8N1
+  - Parses simple framed messages by leading ID byte:
+        0xA0 -> fixed 21-byte frame (including ID)  (log all 21 bytes on ONE line)
+        0xA1 -> fixed 2-byte frame  (ID + 1 data byte)
+    Unknown ID: log single byte and advance (so we don't get stuck)
+*/
 
 class K1UartComponent : public Component {
  public:
@@ -15,7 +27,6 @@ class K1UartComponent : public Component {
   void loop() override;
   void dump_config() override;
 
-  // Changed: use HARDWARE (or remove this method)
   float get_setup_priority() const override {
     return setup_priority::HARDWARE;
   }
@@ -31,7 +42,25 @@ class K1UartComponent : public Component {
   static constexpr int PIN_RX = 32;
   static constexpr int READ_CHUNK = 128;
 
-  void log_bytes_(const uint8_t *data, int len);
+  // Ring buffer for incremental parsing
+  static constexpr size_t RING_CAP = 256;
+  std::array<uint8_t, RING_CAP> ring_{};
+  size_t ring_head_{0}; // write
+  size_t ring_tail_{0}; // read
+  bool ring_full_{false};
+
+  void push_byte_(uint8_t b);
+  bool available_() const;
+  size_t size_() const;
+  uint8_t peek_(size_t offset = 0) const;
+  void pop_(size_t n);
+  void parse_frames_();
+  void log_frame_(const uint8_t *data, size_t len, uint8_t id);
+
+  static constexpr uint8_t ID_A0 = 0xA0;
+  static constexpr uint8_t ID_A1 = 0xA1;
+  static constexpr size_t LEN_A0 = 21;
+  static constexpr size_t LEN_A1 = 2;
 #endif
 };
 
