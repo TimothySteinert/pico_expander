4b37396683a368fd3601d274c34ea177efa44445
TimothySteinert <timothy@steinert.net.au>
2025-09-10 15:01:02 +0800

    Update __init__.py

--- components/argb_strip/__init__.py
+++ components/argb_strip/__init__.py
@@ -1,1 +1,33 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome import pins
+from esphome.const import CONF_ID, CONF_PIN
 
+CONF_NUM_LEDS = "num_leds"
+CONF_GROUPS = "groups"
+
+argb_strip_ns = cg.esphome_ns.namespace("argb_strip")
+ARGBStripComponent = argb_strip_ns.class_("ARGBStripComponent", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(ARGBStripComponent),
+        cv.Required(CONF_PIN): pins.internal_gpio_output_pin_schema,
+        cv.Required(CONF_NUM_LEDS): cv.int_range(min=1, max=2048),
+        cv.Required(CONF_GROUPS): cv.Schema(
+            {cv.valid_name(): cv.All(cv.ensure_list(cv.int_range(min=1)), [cv.int_range(min=1)])}
+        ),
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+
+    pin = await cg.gpio_pin_expression(config[CONF_PIN])
+    cg.add(var.set_pin(pin))
+    cg.add(var.set_num_leds(config[CONF_NUM_LEDS]))
+
+    for name, led_list in config[CONF_GROUPS].items():
+        zero_based = [l - 1 for l in led_list]
+        cg.add(var.add_group(cg.std_string(name), zero_based))
