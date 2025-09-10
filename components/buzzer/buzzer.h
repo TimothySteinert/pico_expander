4adfe059469ce55bdc35c02e760cdd40d95c6294
TimothySteinert <timothy@steinert.net.au>
2025-09-10 18:55:28 +0800

    Update buzzer.h

--- components/buzzer/buzzer.h
+++ components/buzzer/buzzer.h
@@ -2,7 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/automation.h"
-#include "esphome/core/hal.h"  // For gpio::GPIOPin
+#include "esphome/core/hal.h"  // GPIOPin
 
 namespace esphome {
 namespace buzzer {
@@ -11,13 +11,14 @@ class BuzzerComponent : public Component {
  public:
   void setup() override {
     if (this->pin_ != nullptr) {
-      this->pin_->setup();          // Configure as output (schema ensures output pin)
-      this->pin_->digital_write(false);  // Ensure off at boot
+      this->pin_->setup();
+      this->pin_->digital_write(false);  // Ensure OFF
     }
   }
+
   void loop() override;
 
-  void set_pin(gpio::GPIOPin *pin) { this->pin_ = pin; }
+  void set_pin(GPIOPin *pin) { this->pin_ = pin; }
 
   void start(uint8_t beeps, uint32_t short_pause, uint32_t long_pause,
              uint8_t tone, bool repeat, uint32_t beep_length = 200);
@@ -28,14 +29,14 @@ class BuzzerComponent : public Component {
  protected:
   void apply_value_(uint8_t value);
 
-  gpio::GPIOPin *pin_{nullptr};
+  GPIOPin *pin_{nullptr};
 
   bool running_{false};
   bool beep_on_{false};
   bool repeat_{false};
   uint8_t beeps_{1};
   uint8_t beeps_done_{0};
-  uint8_t tone_{255};  // Non-zero means ON
+  uint8_t tone_{255};  // Non-zero => ON
   uint32_t short_pause_{100};
   uint32_t long_pause_{500};
   uint32_t beep_length_{200};
@@ -44,7 +45,6 @@ class BuzzerComponent : public Component {
 };
 
 // ---- Actions ----
-
 template<typename... Ts> class StartAction : public Action<Ts...> {
  public:
   explicit StartAction(BuzzerComponent *parent) : parent_(parent) {}
