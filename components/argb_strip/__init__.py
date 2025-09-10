4316f09f4db34c19e7651df451b9a280fa9d300a
TimothySteinert <timothy@steinert.net.au>
2025-09-10 15:07:36 +0800

    Update __init__.py

--- components/argb_strip/__init__.py
+++ components/argb_strip/__init__.py
@@ -14,11 +14,17 @@ CONFIG_SCHEMA = cv.Schema(
         cv.GenerateID(): cv.declare_id(ARGBStripComponent),
         cv.Required(CONF_PIN): pins.internal_gpio_output_pin_schema,
         cv.Required(CONF_NUM_LEDS): cv.int_range(min=1, max=2048),
-        cv.Required(CONF_GROUPS): cv.Schema(
-            {cv.valid_name(): cv.All(cv.ensure_list(cv.int_range(min=1)), [cv.int_range(min=1)])}
-        ),
+        cv.Required(CONF_GROUPS): cv.Schema({
+            # Keys: group names (must be valid identifiers)
+            # Values: list of 1-based LED indices
+            cv.valid_name: cv.All(
+                cv.ensure_list(cv.int_range(min=1)),
+                cv.Length(min=1)
+            )
+        }),
     }
 ).extend(cv.COMPONENT_SCHEMA)
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -26,8 +32,15 @@ async def to_code(config):
 
     pin = await cg.gpio_pin_expression(config[CONF_PIN])
     cg.add(var.set_pin(pin))
-    cg.add(var.set_num_leds(config[CONF_NUM_LEDS]))
+    num_leds = config[CONF_NUM_LEDS]
+    cg.add(var.set_num_leds(num_leds))
 
+    # Validate group LED indices against num_leds
     for name, led_list in config[CONF_GROUPS].items():
+        for led in led_list:
+            if led > num_leds:
+                raise cv.Invalid(
+                    f"Group '{name}' references LED {led}, but num_leds is {num_leds}"
+                )
         zero_based = [l - 1 for l in led_list]
         cg.add(var.add_group(cg.std_string(name), zero_based))
