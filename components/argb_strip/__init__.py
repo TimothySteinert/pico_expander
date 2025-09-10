acdd57853c5db16d817442347d7ff3d12fceab96
TimothySteinert <timothy@steinert.net.au>
2025-09-10 15:26:23 +0800

    Update __init__.py

--- components/argb_strip/__init__.py
+++ components/argb_strip/__init__.py
@@ -5,6 +5,8 @@ from esphome.const import CONF_ID, CONF_PIN
 
 CONF_NUM_LEDS = "num_leds"
 CONF_GROUPS = "groups"
+CONF_GLOBAL_BRIGHTNESS = "global_brightness"
+CONF_GAMMA = "gamma"
 
 argb_strip_ns = cg.esphome_ns.namespace("argb_strip")
 ARGBStripComponent = argb_strip_ns.class_("ARGBStripComponent", cg.Component)
@@ -20,6 +22,8 @@ CONFIG_SCHEMA = cv.Schema(
                 cv.Length(min=1)
             )
         }),
+        cv.Optional(CONF_GLOBAL_BRIGHTNESS, default=1.0): cv.float_range(min=0.0, max=1.0),
+        cv.Optional(CONF_GAMMA, default=False): cv.boolean,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -27,17 +31,16 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    # Standard ESPHome pin expression (still used for high-level ops, direction, etc.)
     pin_expr = await cg.gpio_pin_expression(config[CONF_PIN])
     cg.add(var.set_pin(pin_expr))
-
     num_leds = config[CONF_NUM_LEDS]
     cg.add(var.set_num_leds(num_leds))
 
-    # Extract raw pin number directly from the YAML config
-    # The internal_gpio_output_pin_schema stores it under key 'number'
     raw_pin_num = config[CONF_PIN]['number']
     cg.add(var.set_raw_gpio(raw_pin_num))
+
+    cg.add(var.set_global_brightness(config[CONF_GLOBAL_BRIGHTNESS]))
+    cg.add(var.set_gamma_enabled(config[CONF_GAMMA]))
 
     for name, led_list in config[CONF_GROUPS].items():
         for led in led_list:
