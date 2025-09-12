53feb39872e8f2925f8f118e3fa34278daa99833
TimothySteinert <timothy@steinert.net.au>
2025-09-12 12:09:26 +0800

    Create binary_sensor.py

---
+++ components/k1_alarm_listener/binary_sensor.py
@@ -0,0 +1,37 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import binary_sensor
+from esphome.const import CONF_ID
+
+from . import K1AlarmListener
+
+CONF_K1_ALARM_LISTENER_ID = "k1_alarm_listener_id"
+CONF_TYPE = "type"
+
+# For future expansion, we define types. Currently only 'ha_connected'.
+BINARY_SENSOR_TYPES = {
+    "ha_connected": 0,
+}
+
+K1AlarmListenerBinarySensor = cg.esphome_ns.namespace("k1_alarm_listener").class_(
+    "K1AlarmListenerBinarySensor",
+    binary_sensor.BinarySensor,
+    cg.Component
+)
+
+CONFIG_SCHEMA = binary_sensor.BINARY_SENSOR_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(K1AlarmListenerBinarySensor),
+        cv.GenerateID(CONF_K1_ALARM_LISTENER_ID): cv.use_id(K1AlarmListener),
+        cv.Optional(CONF_TYPE, default="ha_connected"): cv.one_of(*BINARY_SENSOR_TYPES, lower=True),
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+
+async def to_code(config):
+    parent = await cg.get_variable(config[CONF_K1_ALARM_LISTENER_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await binary_sensor.register_binary_sensor(var, config)
+    cg.add(var.set_parent(parent))
+    cg.add(var.set_type(BINARY_SENSOR_TYPES[config[CONF_TYPE]]))
