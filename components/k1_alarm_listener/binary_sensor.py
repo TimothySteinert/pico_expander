c4a0a8c66e866047d7e739d07d8049348888cf94
TimothySteinert <timothy@steinert.net.au>
2025-09-12 13:19:29 +0800

    Update binary_sensor.py

--- components/k1_alarm_listener/binary_sensor.py
+++ components/k1_alarm_listener/binary_sensor.py
@@ -3,33 +3,32 @@ import esphome.config_validation as cv
 from esphome.components import binary_sensor
 from esphome.const import CONF_ID
 
-from . import K1AlarmListener
+from . import k1_alarm_listener_ns, K1AlarmListener
 
 CONF_K1_ALARM_LISTENER_ID = "k1_alarm_listener_id"
 CONF_TYPE = "type"
 
-# Aliases all map to the same enum (0)
-BINARY_SENSOR_TYPES = {
+# Only one type right now, keep extensible
+TYPE_MAP = {
     "ha_connected": 0,
-    "ha_available": 0,
-    "api_connected": 0,
-    "connection": 0,
 }
 
-K1AlarmListenerBinarySensor = cg.esphome_ns.namespace("k1_alarm_listener").class_(
-    "K1AlarmListenerBinarySensor",
-    binary_sensor.BinarySensor,
-    cg.Component
+K1AlarmListenerBinarySensor = k1_alarm_listener_ns.class_(
+    "K1AlarmListenerBinarySensor", binary_sensor.BinarySensor, cg.Component
 )
 
-CONFIG_SCHEMA = binary_sensor.BINARY_SENSOR_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(K1AlarmListenerBinarySensor),
-        cv.GenerateID(CONF_K1_ALARM_LISTENER_ID): cv.use_id(K1AlarmListener),
-        cv.Optional(CONF_TYPE, default="ha_connected"): cv.one_of(*BINARY_SENSOR_TYPES.keys(), lower=True),
-    }
-).extend(cv.COMPONENT_SCHEMA)
-
+CONFIG_SCHEMA = (
+    binary_sensor.binary_sensor_schema(K1AlarmListenerBinarySensor)
+    .extend(
+        {
+            cv.GenerateID(CONF_K1_ALARM_LISTENER_ID): cv.use_id(K1AlarmListener),
+            cv.Optional(CONF_TYPE, default="ha_connected"): cv.one_of(
+                *TYPE_MAP.keys(), lower=True
+            ),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+)
 
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_K1_ALARM_LISTENER_ID])
@@ -37,4 +36,4 @@ async def to_code(config):
     await cg.register_component(var, config)
     await binary_sensor.register_binary_sensor(var, config)
     cg.add(var.set_parent(parent))
-    cg.add(var.set_type(BINARY_SENSOR_TYPES[config[CONF_TYPE]]))
+    cg.add(var.set_type(TYPE_MAP[config[CONF_TYPE]]))
