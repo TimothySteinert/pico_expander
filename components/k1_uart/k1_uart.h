9bd5a7867e12b8f371dd0e4bf67e5f408e1bf94d
TimothySteinert <timothy@steinert.net.au>
2025-09-12 18:01:21 +0800

    Update k1_uart.h

--- components/k1_uart/k1_uart.h
+++ components/k1_uart/k1_uart.h
@@ -4,32 +4,37 @@
 
 #ifdef USE_ESP32
 #include "driver/uart.h"
+#include "esphome/components/script/script.h"
 #endif
 
 #include <array>
 #include <cstdint>
 #include <string>
-#include <vector>
 
 namespace esphome {
 namespace buzzer { class BuzzerComponent; }
-namespace script { class Script; class ScriptExecutor; }
 
 namespace k1_uart {
+
+#ifdef USE_ESP32
+// Script signature: (pin, force, skip_delay)
+using AlarmScript = script::Script<std::string, bool, bool>;
+#endif
 
 class K1UartComponent : public Component {
  public:
   void setup() override;
   void loop() override;
   void dump_config() override;
-
   float get_setup_priority() const override { return setup_priority::HARDWARE; }
 
   void set_buzzer(esphome::buzzer::BuzzerComponent *b) { buzzer_ = b; }
 
-  void set_away_script(esphome::script::Script *s) { away_script_ = s; }
-  void set_home_script(esphome::script::Script *s) { home_script_ = s; }
-  void set_disarm_script(esphome::script::Script *s) { disarm_script_ = s; }
+#ifdef USE_ESP32
+  void set_away_script(AlarmScript *s) { away_script_ = s; }
+  void set_home_script(AlarmScript *s) { home_script_ = s; }
+  void set_disarm_script(AlarmScript *s) { disarm_script_ = s; }
+#endif
 
   void set_force_prefix(const std::string &v) { force_prefix_ = v; }
   void set_skip_delay_prefix(const std::string &v) { skip_delay_prefix_ = v; }
@@ -46,18 +51,18 @@ class K1UartComponent : public Component {
   static constexpr int PIN_RX = 32;
   static constexpr int READ_CHUNK = 128;
 
+  // Frame specs
+  static constexpr uint8_t ID_A0 = 0xA0;
+  static constexpr uint8_t ID_A1 = 0xA1;
+  static constexpr size_t LEN_A0 = 21;
+  static constexpr size_t LEN_A1 = 2;
+
   // Ring buffer
   static constexpr size_t RING_CAP = 256;
   std::array<uint8_t, RING_CAP> ring_{};
   size_t ring_head_{0};
   size_t ring_tail_{0};
   bool ring_full_{false};
-
-  // Frame IDs & lengths
-  static constexpr uint8_t ID_A0 = 0xA0;
-  static constexpr uint8_t ID_A1 = 0xA1;
-  static constexpr size_t LEN_A0 = 21;
-  static constexpr size_t LEN_A1 = 2;
 
   // Buffer helpers
   void push_byte_(uint8_t b);
@@ -71,29 +76,25 @@ class K1UartComponent : public Component {
   void handle_a0_(const uint8_t *frame, size_t len);
   void log_frame_(const uint8_t *data, size_t len, uint8_t id);
 
-  // Digit & arm select mapping (from your previous handler)
+  // Mapping
   std::string map_digit_(uint8_t code) const;
   const char *map_arm_select_(uint8_t code) const;
 
-  // Script invocation
-  void call_arm_script_(esphome::script::Script *script,
-                        const std::string &pin,
-                        bool force_flag,
-                        bool skip_flag);
-  void call_disarm_script_(esphome::script::Script *script,
-                           const std::string &prefix,
-                           const std::string &pin,
-                           bool force_flag,
-                           bool skip_flag);
+  // Execute script (pin, force_flag, skip_flag)
+  void exec_script_(AlarmScript *script,
+                    const std::string &pin,
+                    bool force_flag,
+                    bool skip_flag);
 #endif
 
-  // Linked components/scripts
   esphome::buzzer::BuzzerComponent *buzzer_{nullptr};
-  esphome::script::Script *away_script_{nullptr};
-  esphome::script::Script *home_script_{nullptr};
-  esphome::script::Script *disarm_script_{nullptr};
 
-  // Prefix rules
+#ifdef USE_ESP32
+  AlarmScript *away_script_{nullptr};
+  AlarmScript *home_script_{nullptr};
+  AlarmScript *disarm_script_{nullptr};
+#endif
+
   std::string force_prefix_{"999"};
   std::string skip_delay_prefix_{"998"};
 };
