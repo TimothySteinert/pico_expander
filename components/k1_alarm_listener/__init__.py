2c9c32b33aaf8c5f88aab137efd00748564f4b17
TimothySteinert <timothy@steinert.net.au>
2025-09-12 11:30:59 +0800

    Update __init__.py

--- components/k1_alarm_listener/__init__.py
+++ components/k1_alarm_listener/__init__.py
@@ -1,17 +1,20 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
-from esphome import automation
+from esphome.components import api  # <-- needed for CustomAPIDevice
 
 k1_alarm_listener_ns = cg.esphome_ns.namespace("k1_alarm_listener")
-K1AlarmListener = k1_alarm_listener_ns.class_("K1AlarmListener", cg.Component, cg.CustomAPIDevice)
+K1AlarmListener = k1_alarm_listener_ns.class_("K1AlarmListener", cg.Component, api.CustomAPIDevice)
 
 CONF_ALARM_ENTITY = "alarm_entity"
+
+# Ensure api: is present in YAML
+DEPENDENCIES = ["api"]
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(K1AlarmListener),
-        cv.Required(CONF_ALARM_ENTITY): cv.string,  # could further validate with cv.entity_id
+        cv.Required(CONF_ALARM_ENTITY): cv.string,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -19,5 +22,4 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    # CustomAPIDevice doesn't need explicit registration beyond inheritance
     cg.add(var.set_alarm_entity(config[CONF_ALARM_ENTITY]))
