6df319973e9b94a4cdc73801f9874faf30bcf97b
TimothySteinert <timothy@steinert.net.au>
2025-09-12 13:05:05 +0800

    Update __init__.py

--- components/k1_alarm_listener/__init__.py
+++ components/k1_alarm_listener/__init__.py
@@ -7,6 +7,8 @@ K1AlarmListener = k1_alarm_listener_ns.class_("K1AlarmListener", cg.Component)
 
 CONF_ALARM_ENTITY = "alarm_entity"
 CONF_ALARM_TYPE = "alarm_type"
+CONF_INCORRECT_PIN_TIMEOUT = "incorrect_pin_timeout"
+CONF_FAILED_OPEN_SENSORS_TIMEOUT = "failed_open_sensors_timeout"
 
 ALARM_TYPES = {
     "alarmo": 0,
@@ -18,6 +20,8 @@ CONFIG_SCHEMA = cv.Schema(
         cv.GenerateID(): cv.declare_id(K1AlarmListener),
         cv.Required(CONF_ALARM_ENTITY): cv.string,
         cv.Optional(CONF_ALARM_TYPE, default="alarmo"): cv.one_of(*ALARM_TYPES.keys(), lower=True),
+        cv.Optional(CONF_INCORRECT_PIN_TIMEOUT, default="2s"): cv.positive_time_period_milliseconds,
+        cv.Optional(CONF_FAILED_OPEN_SENSORS_TIMEOUT, default="2s"): cv.positive_time_period_milliseconds,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -27,3 +31,5 @@ async def to_code(config):
     await cg.register_component(var, config)
     cg.add(var.set_alarm_entity(config[CONF_ALARM_ENTITY]))
     cg.add(var.set_alarm_type(ALARM_TYPES[config[CONF_ALARM_TYPE]]))
+    cg.add(var.set_incorrect_pin_timeout_ms(config[CONF_INCORRECT_PIN_TIMEOUT]))
+    cg.add(var.set_failed_open_sensors_timeout_ms(config[CONF_FAILED_OPEN_SENSORS_TIMEOUT]))
