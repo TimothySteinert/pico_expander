2c508800d0762e3be388d3eb6bc51ae002867d4a
TimothySteinert <timothy@steinert.net.au>
2025-09-12 14:38:03 +0800

    Update __init__.py

--- components/test_states/__init__.py
+++ components/test_states/__init__.py
@@ -8,10 +8,8 @@ test_states_ns = cg.esphome_ns.namespace("test_states")
 TestStatesComponent = test_states_ns.class_("TestStatesComponent", cg.Component)
 ModeTrigger = test_states_ns.class_("ModeTrigger", automation.Trigger)
 
-# We hard-code 3 modes for this test component.
 STATE_KEYS = ["mode1", "mode2", "mode3"]
 
-# Each mode key can contain a normal ESPHome automation (list of actions)
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(TestStatesComponent),
@@ -26,10 +24,13 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    # For each optional mode automation list, create a trigger and attach actions
+    # For each configured mode block, create a trigger with a fresh internal ID
     for key in STATE_KEYS:
         if key in config:
-            trig = cg.new_Pvariable(ModeTrigger)
+            trig_id = cg.generate_id(f"{config[CONF_ID].id}_{key}_trig")
+            trig = cg.new_Pvariable(trig_id, ModeTrigger)
+            # Attach the trigger to the component
             cg.add(getattr(var, f"set_{key}_trigger")(trig))
+            # Build each automation entry onto this trigger
             for conf in config[key]:
                 await automation.build_automation(trig, [], conf)
