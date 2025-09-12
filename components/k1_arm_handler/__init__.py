c3d46d8f0c13f5966965d9ac26ef497933e1b9a6
TimothySteinert <timothy@steinert.net.au>
2025-09-12 11:00:31 +0800

    Update __init__.py

--- components/k1_arm_handler/__init__.py
+++ components/k1_arm_handler/__init__.py
@@ -1,24 +1,23 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.components import script
 
 k1_arm_handler_ns = cg.esphome_ns.namespace("k1_arm_handler")
 K1ArmHandlerComponent = k1_arm_handler_ns.class_("K1ArmHandlerComponent", cg.Component)
 
 CONF_ID = "id"
-CONF_ALARM_ENTITY_ID = "alarm_entity_id"
-CONF_ARM_SERVICE = "arm_service"
-CONF_DISARM_SERVICE = "disarm_service"
+CONF_ALARM_ENTITY_ID = "alarm_entity_id"     # kept for reference/logging (not used internally now)
 CONF_FORCE_PREFIX = "force_prefix"
 CONF_SKIP_DELAY_PREFIX = "skip_delay_prefix"
+CONF_CALLBACK_SCRIPT = "callback_script"
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(K1ArmHandlerComponent),
-        cv.Required(CONF_ALARM_ENTITY_ID): cv.entity_id,
-        cv.Optional(CONF_ARM_SERVICE, default="alarmo.arm"): cv.string,
-        cv.Optional(CONF_DISARM_SERVICE, default="alarm_control_panel.alarm_disarm"): cv.string,
+        cv.Optional(CONF_ALARM_ENTITY_ID, default=""): cv.string,
         cv.Optional(CONF_FORCE_PREFIX, default="999"): cv.string,
         cv.Optional(CONF_SKIP_DELAY_PREFIX, default="998"): cv.string,
+        cv.Required(CONF_CALLBACK_SCRIPT): cv.use_id(script.Script),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -27,7 +26,8 @@ async def to_code(config):
     await cg.register_component(var, config)
 
     cg.add(var.set_alarm_entity_id(config[CONF_ALARM_ENTITY_ID]))
-    cg.add(var.set_arm_service(config[CONF_ARM_SERVICE]))
-    cg.add(var.set_disarm_service(config[CONF_DISARM_SERVICE]))
     cg.add(var.set_force_prefix(config[CONF_FORCE_PREFIX]))
     cg.add(var.set_skip_delay_prefix(config[CONF_SKIP_DELAY_PREFIX]))
+
+    scr = await cg.get_variable(config[CONF_CALLBACK_SCRIPT])
+    cg.add(var.set_callback_script(scr))
