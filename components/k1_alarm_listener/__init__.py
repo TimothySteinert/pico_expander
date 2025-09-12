e09aabc5e843e2cdcf05de27ef61698510938bd3
TimothySteinert <timothy@steinert.net.au>
2025-09-12 11:33:41 +0800

    Update __init__.py

--- components/k1_alarm_listener/__init__.py
+++ components/k1_alarm_listener/__init__.py
@@ -1,14 +1,16 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
-from esphome.components import api  # <-- needed for CustomAPIDevice
 
 k1_alarm_listener_ns = cg.esphome_ns.namespace("k1_alarm_listener")
-K1AlarmListener = k1_alarm_listener_ns.class_("K1AlarmListener", cg.Component, api.CustomAPIDevice)
+# We only list cg.Component here. The C++ class will still inherit api::CustomAPIDevice
+# in the header; Python doesn't need to know that (avoids AttributeError on some versions).
+K1AlarmListener = k1_alarm_listener_ns.class_("K1AlarmListener", cg.Component)
 
 CONF_ALARM_ENTITY = "alarm_entity"
 
-# Ensure api: is present in YAML
+# Ensure api: is present (and user must set homeassistant_states: true in YAML
+# because we're calling subscribe_homeassistant_state() directly).
 DEPENDENCIES = ["api"]
 
 CONFIG_SCHEMA = cv.Schema(
