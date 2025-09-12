9f2ebdddc720eb992e1697cd591026a23f86d844
TimothySteinert <timothy@steinert.net.au>
2025-09-12 12:31:44 +0800

    Update __init__.py

--- components/k1_alarm_listener/__init__.py
+++ components/k1_alarm_listener/__init__.py
@@ -6,13 +6,18 @@ k1_alarm_listener_ns = cg.esphome_ns.namespace("k1_alarm_listener")
 K1AlarmListener = k1_alarm_listener_ns.class_("K1AlarmListener", cg.Component)
 
 CONF_ALARM_ENTITY = "alarm_entity"
+CONF_ALARM_TYPE = "alarm_type"
 
-DEPENDENCIES = ["api"]
+ALARM_TYPES = {
+    "alarmo": 0,
+    "other": 1,
+}
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(K1AlarmListener),
         cv.Required(CONF_ALARM_ENTITY): cv.string,
+        cv.Optional(CONF_ALARM_TYPE, default="alarmo"): cv.one_of(*ALARM_TYPES.keys(), lower=True),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -21,3 +26,4 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     cg.add(var.set_alarm_entity(config[CONF_ALARM_ENTITY]))
+    cg.add(var.set_alarm_type(ALARM_TYPES[config[CONF_ALARM_TYPE]]))
