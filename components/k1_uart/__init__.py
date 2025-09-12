bae618d837f337a47ab01bc56618e4d114e0eabe
TimothySteinert <timothy@steinert.net.au>
2025-09-12 10:51:11 +0800

    Update __init__.py

--- components/k1_uart/__init__.py
+++ components/k1_uart/__init__.py
@@ -5,17 +5,22 @@ from esphome.const import CONF_ID
 k1_uart_ns = cg.esphome_ns.namespace("k1_uart")
 K1UartComponent = k1_uart_ns.class_("K1UartComponent", cg.Component)
 
-# Reference the existing external buzzer component (no code duplication,
-# just a type handle so we can accept an ID)
+# Existing buzzer reference
 buzzer_ns = cg.esphome_ns.namespace("buzzer")
 BuzzerComponent = buzzer_ns.class_("BuzzerComponent", cg.Component)
 
+# New: arm handler reference
+k1_arm_handler_ns = cg.esphome_ns.namespace("k1_arm_handler")
+K1ArmHandlerComponent = k1_arm_handler_ns.class_("K1ArmHandlerComponent", cg.Component)
+
 CONF_BUZZER_ID = "buzzer_id"
+CONF_ARM_HANDLER_ID = "arm_handler_id"
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(K1UartComponent),
         cv.Optional(CONF_BUZZER_ID): cv.use_id(BuzzerComponent),
+        cv.Optional(CONF_ARM_HANDLER_ID): cv.use_id(K1ArmHandlerComponent),
     }
 )
 
@@ -24,4 +29,7 @@ async def to_code(config):
     if CONF_BUZZER_ID in config:
         buz = await cg.get_variable(config[CONF_BUZZER_ID])
         cg.add(var.set_buzzer(buz))
+    if CONF_ARM_HANDLER_ID in config:
+        ah = await cg.get_variable(config[CONF_ARM_HANDLER_ID])
+        cg.add(var.set_arm_handler(ah))
     await cg.register_component(var, config)
