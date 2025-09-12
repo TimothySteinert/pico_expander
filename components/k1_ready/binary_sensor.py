ce43d8eaf3f0b55184f098e8a1ab5b948c67dc22
TimothySteinert <timothy@steinert.net.au>
2025-09-12 14:14:49 +0800

    Update binary_sensor.py

--- components/k1_ready/binary_sensor.py
+++ components/k1_ready/binary_sensor.py
@@ -3,9 +3,8 @@ import esphome.config_validation as cv
 from esphome.components import binary_sensor
 from esphome.const import CONF_ID
 
-from . import k1_ready_ns
+from . import k1_ready_ns, K1Ready
 
-K1ReadySelect = k1_ready_ns.class_("K1ReadySelect", cg.Component)
 K1ReadyBinarySensor = k1_ready_ns.class_("K1ReadyBinarySensor", binary_sensor.BinarySensor, cg.Component)
 
 CONF_K1_READY_ID = "k1_ready_id"
@@ -13,14 +12,13 @@ CONF_K1_READY_ID = "k1_ready_id"
 CONFIG_SCHEMA = binary_sensor.binary_sensor_schema(K1ReadyBinarySensor).extend(
     {
         cv.GenerateID(): cv.declare_id(K1ReadyBinarySensor),
-        cv.Required(CONF_K1_READY_ID): cv.use_id(K1ReadySelect),
+        cv.Required(CONF_K1_READY_ID): cv.use_id(K1Ready),
     }
 )
 
 async def to_code(config):
-    parent = await cg.get_variable(config[CONF_K1_READY_ID])
-    var = cg.new_Pvariable(config[CONF_ID], parent)
+    engine = await cg.get_variable(config[CONF_K1_READY_ID])
+    var = cg.new_Pvariable(config[CONF_ID], engine)
     await cg.register_component(var, config)
     await binary_sensor.register_binary_sensor(var, config)
-    # Let parent register us
-    cg.add(parent.register_ready_binary_sensor(var))
+    cg.add(engine.register_ready_binary_sensor(var))
