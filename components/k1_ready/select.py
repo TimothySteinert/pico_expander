2d0acad2b1789a1b38841aef4674f310db55eb3d
TimothySteinert <timothy@steinert.net.au>
2025-09-12 14:14:32 +0800

    Update select.py

--- components/k1_ready/select.py
+++ components/k1_ready/select.py
@@ -1,54 +1,25 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import select, api
+from esphome.components import select
 from esphome.const import CONF_ID
 
-from . import k1_ready_ns
+from . import k1_ready_ns, K1Ready
 
-# Correct: use api.CustomAPIDevice (NOT cg.api.CustomAPIDevice)
-K1ReadySelect = k1_ready_ns.class_("K1ReadySelect", select.Select, cg.Component, api.CustomAPIDevice)
+K1ReadySelect = k1_ready_ns.class_("K1ReadySelect", select.Select, cg.Component)
 
-CONF_OPTIONS = "options"
-CONF_INITIAL_OPTION = "initial_option"
-CONF_OPTIMISTIC = "optimistic"
-CONF_RESTORE_VALUE = "restore_value"
+CONF_K1_READY_ID = "k1_ready_id"
 
-CONFIG_SCHEMA = (
-    select.select_schema(K1ReadySelect)
-    .extend(
-        {
-            cv.Required(CONF_OPTIONS): cv.All(
-                cv.ensure_list(cv.string),
-                cv.Length(min=1),
-            ),
-            cv.Optional(CONF_INITIAL_OPTION): cv.string,
-            cv.Optional(CONF_OPTIMISTIC, default=True): cv.boolean,
-            cv.Optional(CONF_RESTORE_VALUE, default=True): cv.boolean,
-        }
-    )
-    .add_extra_validation(
-        lambda cfg: (
-            cv.Schema({cv.Optional(CONF_INITIAL_OPTION): cv.In(cfg[CONF_OPTIONS])})(cfg)
-            if CONF_INITIAL_OPTION in cfg
-            else cfg
-        )
-    )
+CONFIG_SCHEMA = select.select_schema(K1ReadySelect).extend(
+    {
+        cv.GenerateID(): cv.declare_id(K1ReadySelect),
+        cv.Required(CONF_K1_READY_ID): cv.use_id(K1Ready),
+    }
 )
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-
-    for opt in config[CONF_OPTIONS]:
-        cg.add(var.add_option(opt))
-
-    if CONF_INITIAL_OPTION in config:
-        cg.add(var.set_initial_option(config[CONF_INITIAL_OPTION]))
-
-    cg.add(var.set_optimistic(config[CONF_OPTIMISTIC]))
-    cg.add(var.set_restore_value(config[CONF_RESTORE_VALUE]))
-
+    engine = await cg.get_variable(config[CONF_K1_READY_ID])
+    var = cg.new_Pvariable(config[CONF_ID], engine)
     await cg.register_component(var, config)
     await select.register_select(var, config)
-
-    # Register the ready_update API service
-    cg.add(var.register_ready_update_service())
+    # Bind so engine can push its current selection immediately
+    cg.add(var.bind_to_engine())
