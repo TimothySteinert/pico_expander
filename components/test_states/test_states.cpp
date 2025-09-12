557ae53ed1d1fb4b89a7e7b68402cb1413291a8c
TimothySteinert <timothy@steinert.net.au>
2025-09-12 14:36:29 +0800

    Update test_states.cpp

--- components/test_states/test_states.cpp
+++ components/test_states/test_states.cpp
@@ -1,5 +1,6 @@
 #include "test_states.h"
 #include "esphome/core/log.h"
+#include <algorithm>
 
 namespace esphome {
 namespace test_states {
@@ -9,12 +10,29 @@ static const char *const TAG = "test_states";
 void TestStatesComponent::dump_config() {
   ESP_LOGCONFIG(TAG, "Test States Component:");
   ESP_LOGCONFIG(TAG, "  Current Mode: %s", this->mode_string().c_str());
+  ESP_LOGCONFIG(TAG, "  Triggers attached:");
+  ESP_LOGCONFIG(TAG, "    mode1: %s", mode1_trigger_ ? "YES" : "NO");
+  ESP_LOGCONFIG(TAG, "    mode2: %s", mode2_trigger_ ? "YES" : "NO");
+  ESP_LOGCONFIG(TAG, "    mode3: %s", mode3_trigger_ ? "YES" : "NO");
 }
 
 void TestStatesComponent::set_mode(Mode m) {
   if (m == this->mode_) return;
   this->mode_ = m;
-  ESP_LOGD(TAG, "Mode changed to: %s", this->mode_string().c_str());
+  ESP_LOGI(TAG, "Mode changed -> %s", this->mode_string().c_str());
+  // For this first test we just log per your request
+  switch (mode_) {
+    case Mode::MODE1:
+      ESP_LOGI(TAG, "I'm in mode 1");
+      break;
+    case Mode::MODE2:
+      ESP_LOGI(TAG, "I'm in mode 2");
+      break;
+    case Mode::MODE3:
+      ESP_LOGI(TAG, "I'm in mode 3");
+      break;
+  }
+  fire_current_mode_trigger_();
 }
 
 void TestStatesComponent::set_mode_by_name(const std::string &name_in) {
@@ -34,6 +52,20 @@ std::string TestStatesComponent::mode_string() const {
     case Mode::MODE2: return "mode2";
     case Mode::MODE3: return "mode3";
     default: return "unknown";
+  }
+}
+
+void TestStatesComponent::fire_current_mode_trigger_() {
+  switch (mode_) {
+    case Mode::MODE1:
+      if (mode1_trigger_) mode1_trigger_->trigger();
+      break;
+    case Mode::MODE2:
+      if (mode2_trigger_) mode2_trigger_->trigger();
+      break;
+    case Mode::MODE3:
+      if (mode3_trigger_) mode3_trigger_->trigger();
+      break;
   }
 }
 
