f0fb95e99878f0e3725e892d354f0fcb3a549ca6
TimothySteinert <timothy@steinert.net.au>
2025-09-12 12:05:36 +0800

    Update __init__.py

--- components/k1_alarm_listener/__init__.py
+++ components/k1_alarm_listener/__init__.py
@@ -1,22 +1,21 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
+from esphome.components import binary_sensor
 
 k1_alarm_listener_ns = cg.esphome_ns.namespace("k1_alarm_listener")
-# We only list cg.Component here. The C++ class will still inherit api::CustomAPIDevice
-# in the header; Python doesn't need to know that (avoids AttributeError on some versions).
 K1AlarmListener = k1_alarm_listener_ns.class_("K1AlarmListener", cg.Component)
 
 CONF_ALARM_ENTITY = "alarm_entity"
+CONF_HA_STATUS_ID = "ha_status_id"
 
-# Ensure api: is present (and user must set homeassistant_states: true in YAML
-# because we're calling subscribe_homeassistant_state() directly).
-DEPENDENCIES = ["api"]
+DEPENDENCIES = ["api"]  # require api:
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(K1AlarmListener),
         cv.Required(CONF_ALARM_ENTITY): cv.string,
+        cv.Optional(CONF_HA_STATUS_ID): cv.use_id(binary_sensor.BinarySensor),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -25,3 +24,7 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     cg.add(var.set_alarm_entity(config[CONF_ALARM_ENTITY]))
+
+    if CONF_HA_STATUS_ID in config:
+        ha_status = await cg.get_variable(config[CONF_HA_STATUS_ID])
+        cg.add(var.set_ha_status(ha_status))
