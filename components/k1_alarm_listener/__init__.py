a3ff79330cdb13963d8b6d2323f04c4dd09578fd
TimothySteinert <timothy@steinert.net.au>
2025-09-12 11:25:53 +0800

    Create __init__.py

---
+++ components/k1_alarm_listener/__init__.py
@@ -0,0 +1,23 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+from esphome import automation
+
+k1_alarm_listener_ns = cg.esphome_ns.namespace("k1_alarm_listener")
+K1AlarmListener = k1_alarm_listener_ns.class_("K1AlarmListener", cg.Component, cg.CustomAPIDevice)
+
+CONF_ALARM_ENTITY = "alarm_entity"
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(K1AlarmListener),
+        cv.Required(CONF_ALARM_ENTITY): cv.string,  # could further validate with cv.entity_id
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    # CustomAPIDevice doesn't need explicit registration beyond inheritance
+    cg.add(var.set_alarm_entity(config[CONF_ALARM_ENTITY]))
