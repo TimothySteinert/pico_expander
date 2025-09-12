2fc8cd93e64d5e7146150d66f6197bc06f0bc60c
TimothySteinert <timothy@steinert.net.au>
2025-09-12 14:52:42 +0800

    Update test_states.h

--- components/test_states/test_states.h
+++ components/test_states/test_states.h
@@ -12,7 +12,6 @@ namespace test_states {
 
 class TestStatesComponent;
 
-// Trigger fired when entering a given mode name
 class ModeTrigger : public Trigger<> {
  public:
   explicit ModeTrigger(TestStatesComponent *parent) : parent_(parent) {}
@@ -20,18 +19,15 @@ class ModeTrigger : public Trigger<> {
   TestStatesComponent *parent_;
 };
 
-// Main component
 class TestStatesComponent : public Component {
  public:
   void setup() override {}
   void loop() override {}
   void dump_config() override;
 
-  // Set mode (public APIs)
   void set_mode_by_name(const std::string &name);
   const std::string &current_mode_string() const { return current_mode_; }
 
-  // Called from codegen to register triggers
   void add_mode_trigger(const std::string &mode, ModeTrigger *t);
 
  protected:
@@ -41,15 +37,12 @@ class TestStatesComponent : public Component {
   void fire_mode_(const std::string &mode);
 };
 
-// Custom action to set mode
 template<typename... Ts>
 class SetModeAction : public Action<Ts...> {
  public:
   explicit SetModeAction(TestStatesComponent *parent) : parent_(parent) {}
   void set_mode(const std::string &m) { mode_ = m; }
-  void play(Ts... x) override {
-    parent_->set_mode_by_name(mode_);
-  }
+  void play(Ts... x) override { parent_->set_mode_by_name(mode_); }
  protected:
   TestStatesComponent *parent_;
   std::string mode_;
