2eefde9b6ba6fc4c6c75e7ee05596d0b873afaec
TimothySteinert <timothy@steinert.net.au>
2025-09-12 12:05:52 +0800

    Update k1_alarm_listener.h

--- components/k1_alarm_listener/k1_alarm_listener.h
+++ components/k1_alarm_listener/k1_alarm_listener.h
@@ -3,6 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/api/custom_api_device.h"
 #include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
 
 namespace esphome {
 namespace k1_alarm_listener {
@@ -13,6 +14,7 @@ class K1AlarmListener : public Component, public api::CustomAPIDevice {
  public:
   void set_alarm_entity(const std::string &e) { alarm_entity_ = e; }
   void set_text_sensor(K1AlarmListenerTextSensor *ts) { state_sensor_ = ts; }
+  void set_ha_status(binary_sensor::BinarySensor *bs) { ha_status_sensor_ = bs; }
 
   void setup() override;
   void dump_config() override;
@@ -21,11 +23,19 @@ class K1AlarmListener : public Component, public api::CustomAPIDevice {
  protected:
   std::string alarm_entity_;
   K1AlarmListenerTextSensor *state_sensor_{nullptr};
+  binary_sensor::BinarySensor *ha_status_sensor_{nullptr};
   bool subscription_started_{false};
 
-  void ha_state_callback_(std::string state);
+  std::string last_published_state_;
 
-  // (Optional future use) validation helper
+  // Callbacks
+  void ha_state_callback_(std::string state);
+  void ha_status_changed_(float);  // signature matches binary_sensor callback (float state)
+
+  // Helpers
+  bool is_ha_connected_() const;
+  void evaluate_connection_state_();
+  std::string map_alarm_state_(const std::string &raw) const;
   bool is_known_alarm_state_(const std::string &s) const;
 };
 
