5e33ddf164c082be8cc4791e9b4e949e69581d05
TimothySteinert <timothy@steinert.net.au>
2025-09-12 14:48:45 +0800

    Update __init__.py

--- components/test_states/__init__.py
+++ components/test_states/__init__.py
@@ -3,32 +3,62 @@ import esphome.config_validation as cv
 from esphome import automation
 from esphome.const import CONF_ID
 
+# Namespace & classes
 test_states_ns = cg.esphome_ns.namespace("test_states")
 
 TestStatesComponent = test_states_ns.class_("TestStatesComponent", cg.Component)
 ModeTrigger = test_states_ns.class_("ModeTrigger", automation.Trigger)
+SetModeAction = test_states_ns.class_("SetModeAction", automation.Action)
 
-STATE_KEYS = ["mode1", "mode2", "mode3"]
+CONF_MODES = "modes"
+CONF_MODE = "mode"
 
-# We allow exactly one automation block per mode (single=True). User supplies a plain list of actions.
-CONFIG_SCHEMA = cv.Schema(
+# Allow multiple component instances
+MULTI_CONF = True
+
+# Each mode name -> list of automation blocks
+MODES_SCHEMA = cv.Schema(
+    {
+        cv.string: cv.All(cv.ensure_list(automation.validate_automation()))
+    }
+)
+
+COMPONENT_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(TestStatesComponent),
-        cv.Optional("mode1"): automation.validate_automation(single=True),
-        cv.Optional("mode2"): automation.validate_automation(single=True),
-        cv.Optional("mode3"): automation.validate_automation(single=True),
+        cv.Optional(CONF_MODES, default={}): MODES_SCHEMA,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
+CONFIG_SCHEMA = cv.All(cv.ensure_list(COMPONENT_SCHEMA))
 
-    # For each mode that has an automation block, create a ModeTrigger and attach it.
-    # automation.validate_automation(single=True) returns a list (0 or 1 items)
-    for key in STATE_KEYS:
-        for conf in config.get(key, []):
-            # Create a trigger (constructor takes parent component)
-            trig = cg.new_Pvariable(conf[automation.CONF_TRIGGER_ID], var)
-            cg.add(getattr(var, f"add_{key}_trigger")(trig))
-            await automation.build_automation(trig, [], conf)
+@automation.register_action(
+    "test_states.set_mode",
+    SetModeAction,
+    cv.Schema(
+        {
+            cv.Required(CONF_ID): cv.use_id(TestStatesComponent),
+            cv.Required(CONF_MODE): cv.templatable(cv.string_strict),
+        }
+    ),
+)
+async def test_states_set_mode_to_code(config, action_id, template_arg, args):
+    parent = await cg.get_variable(config[CONF_ID])
+    action = cg.new_Pvariable(action_id, template_arg, parent)
+    mode_expr = await cg.templatable(config[CONF_MODE], args, cg.std_string)
+    cg.add(action.set_mode(mode_expr))
+    return action
+
+
+async def to_code(config):
+    for comp_conf in config:
+        var = cg.new_Pvariable(comp_conf[CONF_ID])
+        await cg.register_component(var, comp_conf)
+
+        modes = comp_conf.get(CONF_MODES, {})
+        # For each mode, create triggers
+        for mode_name, automation_list in modes.items():
+            for aut in automation_list:
+                trig = cg.new_Pvariable(aut[automation.CONF_TRIGGER_ID], var)
+                cg.add(var.add_mode_trigger(mode_name, trig))
+                await automation.build_automation(trig, [], aut)
