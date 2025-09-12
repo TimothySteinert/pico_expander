99d3dbb73134f38d06fb4b9a713dfaddaafbb3f0
TimothySteinert <timothy@steinert.net.au>
2025-09-12 14:40:43 +0800

    Update test_states.h

--- components/test_states/test_states.h
+++ components/test_states/test_states.h
@@ -1,17 +1,22 @@
 #pragma once
-
 #include "esphome/core/component.h"
-#include "esphome/core/log.h"
 #include "esphome/core/automation.h"
+#include "esphome/core/log.h"
+#include <vector>
 #include <string>
 #include <cstdint>
 
 namespace esphome {
 namespace test_states {
 
+class TestStatesComponent;  // forward
+
+// A simple trigger tied to a parent component (not strictly needed now, but extensible later)
 class ModeTrigger : public Trigger<> {
  public:
-  // Empty; standard Trigger<> is enough.
+  explicit ModeTrigger(TestStatesComponent *parent) : parent_(parent) {}
+ protected:
+  TestStatesComponent *parent_;
 };
 
 class TestStatesComponent : public Component {
@@ -26,27 +31,26 @@ class TestStatesComponent : public Component {
   void loop() override {}
   void dump_config() override;
 
-  // Public API to change modes
+  // Change mode
   void set_mode(Mode m);
   void set_mode_by_name(const std::string &name);
 
   Mode mode() const { return mode_; }
   std::string mode_string() const;
 
-  // Setters for triggers (called from codegen)
-  void set_mode1_trigger(ModeTrigger *t) { mode1_trigger_ = t; }
-  void set_mode2_trigger(ModeTrigger *t) { mode2_trigger_ = t; }
-  void set_mode3_trigger(ModeTrigger *t) { mode3_trigger_ = t; }
+  // Called from codegen to register triggers
+  void add_mode1_trigger(ModeTrigger *t) { mode1_triggers_.push_back(t); }
+  void add_mode2_trigger(ModeTrigger *t) { mode2_triggers_.push_back(t); }
+  void add_mode3_trigger(ModeTrigger *t) { mode3_triggers_.push_back(t); }
 
  protected:
   Mode mode_{Mode::MODE1};
 
-  // Optional triggers (may be nullptr)
-  ModeTrigger *mode1_trigger_{nullptr};
-  ModeTrigger *mode2_trigger_{nullptr};
-  ModeTrigger *mode3_trigger_{nullptr};
+  std::vector<ModeTrigger*> mode1_triggers_;
+  std::vector<ModeTrigger*> mode2_triggers_;
+  std::vector<ModeTrigger*> mode3_triggers_;
 
-  void fire_current_mode_trigger_();
+  void fire_triggers_for_mode_(Mode m);
 };
 
 }  // namespace test_states
