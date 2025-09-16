887f0cb7f1999964a3cde763b366f7547ed82597
TimothySteinert <timothy@steinert.net.au>
2025-09-16 14:34:26 +0800

    Update __init__.py

--- components/k1_uart/__init__.py
+++ components/k1_uart/__init__.py
@@ -9,13 +9,20 @@ buzzer_ns = cg.esphome_ns.namespace("buzzer")
 BuzzerComponent = buzzer_ns.class_("BuzzerComponent", cg.Component)
 
 script_ns = cg.esphome_ns.namespace("script")
-# We reference parameterized Script (pin:string, force:bool, skip:bool)
-Script3 = script_ns.class_("Script", cg.Component)
+Script3 = script_ns.class_("Script", cg.Component)  # (pin, force, skip_delay)
+
+select_ns = cg.esphome_ns.namespace("select")
+SelectComponent = select_ns.class_("Select", cg.Component)
 
 CONF_BUZZER_ID = "buzzer_id"
 CONF_AWAY_SCRIPT_ID = "away_script_id"
 CONF_HOME_SCRIPT_ID = "home_script_id"
 CONF_DISARM_SCRIPT_ID = "disarm_script_id"
+CONF_NIGHT_SCRIPT_ID = "night_script_id"
+CONF_VACATION_SCRIPT_ID = "vacation_script_id"
+CONF_BYPASS_SCRIPT_ID = "bypass_script_id"
+CONF_MODE_SELECTOR_ID = "mode_selector_id"
+
 CONF_FORCE_PREFIX = "force_prefix"
 CONF_SKIP_DELAY_PREFIX = "skip_delay_prefix"
 
@@ -23,9 +30,17 @@ CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(K1UartComponent),
         cv.Optional(CONF_BUZZER_ID): cv.use_id(BuzzerComponent),
+
         cv.Optional(CONF_AWAY_SCRIPT_ID): cv.use_id(Script3),
         cv.Optional(CONF_HOME_SCRIPT_ID): cv.use_id(Script3),
         cv.Optional(CONF_DISARM_SCRIPT_ID): cv.use_id(Script3),
+
+        cv.Optional(CONF_NIGHT_SCRIPT_ID): cv.use_id(Script3),
+        cv.Optional(CONF_VACATION_SCRIPT_ID): cv.use_id(Script3),
+        cv.Optional(CONF_BYPASS_SCRIPT_ID): cv.use_id(Script3),
+
+        cv.Optional(CONF_MODE_SELECTOR_ID): cv.use_id(SelectComponent),
+
         cv.Optional(CONF_FORCE_PREFIX, default="999"): cv.string,
         cv.Optional(CONF_SKIP_DELAY_PREFIX, default="998"): cv.string,
     }
@@ -41,14 +56,26 @@ async def to_code(config):
     if CONF_AWAY_SCRIPT_ID in config:
         sc = await cg.get_variable(config[CONF_AWAY_SCRIPT_ID])
         cg.add(var.set_away_script(sc))
-
     if CONF_HOME_SCRIPT_ID in config:
         sc = await cg.get_variable(config[CONF_HOME_SCRIPT_ID])
         cg.add(var.set_home_script(sc))
-
     if CONF_DISARM_SCRIPT_ID in config:
         sc = await cg.get_variable(config[CONF_DISARM_SCRIPT_ID])
         cg.add(var.set_disarm_script(sc))
+
+    if CONF_NIGHT_SCRIPT_ID in config:
+        sc = await cg.get_variable(config[CONF_NIGHT_SCRIPT_ID])
+        cg.add(var.set_night_script(sc))
+    if CONF_VACATION_SCRIPT_ID in config:
+        sc = await cg.get_variable(config[CONF_VACATION_SCRIPT_ID])
+        cg.add(var.set_vacation_script(sc))
+    if CONF_BYPASS_SCRIPT_ID in config:
+        sc = await cg.get_variable(config[CONF_BYPASS_SCRIPT_ID])
+        cg.add(var.set_bypass_script(sc))
+
+    if CONF_MODE_SELECTOR_ID in config:
+        sel = await cg.get_variable(config[CONF_MODE_SELECTOR_ID])
+        cg.add(var.set_mode_selector(sel))
 
     cg.add(var.set_force_prefix(config[CONF_FORCE_PREFIX]))
     cg.add(var.set_skip_delay_prefix(config[CONF_SKIP_DELAY_PREFIX]))
